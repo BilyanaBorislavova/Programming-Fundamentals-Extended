da756a880a70bd0fca8a52e1bfce2bbd120367e1
Bilyana Borislavova <happy.bozanko@gmail.com>
2017-12-03 19:38:26 +0200

    100/100

--- More Exercises Arrays/01. Last 3 Equal Strings/01. Last 3 Equal Strings/Program.cs
+++ More Exercises Arrays/01. Last 3 Equal Strings/01. Last 3 Equal Strings/Program.cs
@@ -12,24 +12,23 @@ namespace _01.Last_3_Equal_Strings
         {
             List<string> text = Console.ReadLine()
                  .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                 .Reverse()
                  .ToList();
-
+            string word = "";
 
             for (int i = 0; i < text.Count; i++)
             {
-                int thirdElement = text.Count - 1;
-                int secondElement = thirdElement - 1;
-                int firstElement = secondElement - 1;
-
-                string third = text.ElementAt(thirdElement);
-                string second = text.ElementAt(secondElement);
-                string first = text.ElementAt(firstElement);
-
-                if(third.Equals(second) && second.Equals(first))
+                if(text[i] == text[i + 1] && text[i] == text[i + 2])
                 {
-                    Console.WriteLine();
+                    word = text[i];
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(word + " ");
+                    }
+                    return;
                 }
             }
+
         }
     }
 }
